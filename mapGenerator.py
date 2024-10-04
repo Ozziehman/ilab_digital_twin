38bd012e7f8f4141c01acebb9d91888263abeab1
Oscar Theelen <62102056+Ozziehman@users.noreply.github.com>
2024-10-04 12:08:36 +0200

    Added weather report and cloud coverage image, future plans: real-time simulation data

--- mapGenerator.py
+++ mapGenerator.py
@@ -213,6 +213,188 @@ class JavaScriptInjector:
 
         print(f"camrea simulation ({camera_name}) injected and map saved back as {map_file}\nwith paramaters:\nlat: {camera_latitude} \nlon: {camera_longitude} \ndirection: {direction} \nwidth: {width} \nreach: {reach} meters")
         
+    def inject_weather_report_script(self, map_file, latitude, longitude, api_key_openweathermap):
+        # use open-meteo to get weather report
+        with open(map_file, 'r', encoding='utf-8') as f:
+            html_content = f.read()
+
+        self.weather_report_script = """
+<style>
+    .draggable-window {
+        position: absolute;
+        top: 50px;
+        left: 50px;
+        width: 300px;
+        height: auto;
+        background-color: white;
+        border: 1px solid #4b637f;
+        padding: 10px;
+        z-index: 1000;
+        cursor: move;
+        border-radius: 5px;
+    }
+    .draggable-window-header {
+        background-color: #2271b3;
+        border-radius: 5px 5px 0 0;
+        padding: 10px;
+        cursor: move;
+        z-index: 1001;
+    }
+    .slider-container {
+        margin-top: 10px;
+    }
+    .slider {
+        width: 100%;
+    }
+</style>
+<div id="draggable-window" class="draggable-window" style="top: 95%; left: 0.5%;">
+    <div style="font-weight: bold; text-align: center; color: white" class="draggable-window-header">Weather report</div>
+    <div class="draggable-window-content">
+        <div id="weatherReport">
+            <p>Loading weather data...</p>
+        </div>
+        <div class="slider-container">
+            <input type="range" min="0" max="23" value="0" class="slider" id="hourSlider">
+            <p>Hour: <span id="hourValue">0</span></p>
+        </div>
+    </div>
+</div>
+<script>
+    let weatherData = null;
+
+    async function fetchWeatherReport() {
+        const now = new Date();
+        const comingDay = new Date(now.getTime() + 24 * 60 * 60 * 1000); // time is given in Unix, hence i do the maths to add up a day in milliseconds
+        const currentHourStr = now.toISOString().split('T')[0]; // start time, the API call will get the current time UNTIL the comingDay time, hard to find fitting variable name for the next 24 hours
+        const comingDayStr = comingDay.toISOString().split('T')[0];
+        
+        const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude={{ latitude }}&longitude={{ longitude }}&hourly=temperature_80m,precipitation,precipitation_probability,wind_direction_10m,visibility,weather_code&start_date=${currentHourStr}&end_date=${comingDayStr}&timezone=UTC`;
+
+        const response = await fetch(apiUrl);
+        weatherData = await response.json();
+        displayWeatherReport(0); // hour index 0 means it will show the current hour first
+    }
+
+    function displayWeatherReport(hourIndex) {
+        const weatherReportDiv = document.getElementById('weatherReport');
+        const hourly = weatherData.hourly;
+        // console.log(hourly);
+
+        // calculate actual time for the given hour index
+        const now = new Date();
+        const reportTime = new Date(now.getTime() + hourIndex * 60 * 60 * 1000); //the data given is essentially only each hour but to make it more understandable it will show exact hours from the moment of refresh
+        const reportTimeStr = reportTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        // html with report in it:
+        const reportHtml = `
+            <h2 style="font-weight: bold; text-align: center;">Weather report for ${reportTimeStr}</h2>
+            <p>Temperature: ${hourly.temperature_80m[hourIndex]} Celcius</p>
+            <p>Rain: ${hourly.precipitation[hourIndex]} mm</p>
+            <p>Rain probability: ${hourly.precipitation_probability[hourIndex]}%</p>
+            <p>Wind direction: ${hourly.wind_direction_10m[hourIndex]} degrees</p>
+            <p>Visibility: ${hourly.visibility[hourIndex]} meters</p>
+        `;
+        // replace loading text with contents
+        weatherReportDiv.innerHTML = reportHtml;
+
+        // update the slider and hour display
+        document.getElementById('hourSlider').value = hourIndex;
+        document.getElementById('hourValue').innerText = hourIndex;
+    }
+
+    
+    // to make the window draggable, script that follow mouse cursor when held down
+    dragElement(document.getElementById("draggable-window"));
+
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementsByClassName("draggable-window-header")[0]) {
+            document.getElementsByClassName("draggable-window-header")[0].onmousedown = dragMouseDown;
+        } else {
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+    fetchWeatherReport(); // first initial fetch, would be a shame if there's no data at first
+    
+    // if hour sliders moves update the displayed weather report WITHOUT asking for another API call
+    document.getElementById('hourSlider').addEventListener('input', function() {
+        const hourIndex = parseInt(this.value);
+        displayWeatherReport(hourIndex);
+    });
+
+    // get and display weather data
+    setInterval(fetchWeatherReport, 60000); // refresh every minute
+</script>
+<script>
+    var cloudCoverageLayer = L.featureGroup();
+    cloudCoverageLayer.addTo(mapObject); 
+    L.control.layers(null, { 'Cloud Coverage': cloudCoverageLayer }).addTo(mapObject);
+
+    async function fetchCloudCoverageImage() {
+        const now = new Date();
+        const reportTimeEpoch = Math.floor(now.getTime() / 1000); // convert to UNIX time
+
+        // get zoom of map
+        const zoomLevel = mapObject.getZoom();
+        const latitude = {{ latitude }};
+        const longitude = {{ longitude }};
+
+        // convert to tile coords used by openweathermap
+        const tileSize = 256; // Size of the tile
+        const x = Math.floor((longitude + 180) / 360 * Math.pow(2, zoomLevel));
+        const y = Math.floor((1 - (Math.log(Math.tan(latitude * Math.PI / 180) + 1 / Math.cos(latitude * Math.PI / 180)) / Math.PI)) / 2 * Math.pow(2, zoomLevel));
+
+        // api url with params
+        const apiUrl = `https://tile.openweathermap.org/map/clouds_new/${zoomLevel}/${x}/${y}.png?appid={{ API_KEY_OPENWEATHERMAP }}&forecast=${reportTimeEpoch}`;
+        console.log(apiUrl);
+        
+        // overlay iamg over the map
+        const cloudCoverageOverlay = L.imageOverlay(apiUrl, mapObject.getBounds());
+        cloudCoverageOverlay.addTo(cloudCoverageLayer);
+    }
+
+    fetchCloudCoverageImage(); // initial fetch
+    setInterval(fetchCloudCoverageImage(), 60000); // refresh every minute
+</script>
+    """
+        self.weather_report_script = self.weather_report_script.replace("{{ latitude }}", str(latitude))
+        self.weather_report_script = self.weather_report_script.replace("{{ longitude }}", str(longitude))
+        self.weather_report_script = self.weather_report_script.replace("{{ API_KEY_OPENWEATHERMAP }}", str(api_key_openweathermap))
+
+        # inject weather report into correct spot
+        self.modified_html_weather_report = html_content.replace("</html>", self.weather_report_script + "\n</html>") #modify the html by 
+
+        # save modified html back
+        with open(map_file, 'w', encoding='utf-8') as f:
+            f.write(self.modified_html_weather_report)
+        
+        print(f"weather report js injected and map saved back as {map_file}")
     
 class MapCreator:
     def __init__(self, latitude, longitude, name, load_dist=2000, water_buffer_size = 150, road_buffer_size=20):
@@ -225,7 +407,7 @@ class MapCreator:
         self.road_buffer_size = road_buffer_size
         self.dataDownloader = DataDownloader(self.latitude, self.longitude, dist=self.load_dist)
         self.mapStyler = MapStyler()
-        self.m = folium.Map(location=[self.latitude, self.longitude], zoom_start=12)
+        self.m = folium.Map(location=[self.latitude, self.longitude], zoom_start=8)
         self.javaScriptInjector = JavaScriptInjector()
         self.map_name = f'static/maps/map_{self.name}.html'
 
@@ -255,7 +437,7 @@ class MapCreator:
     
     def add_buildings_tooltips(self, buildings, buildings_fg, styler, extra_data=None):
         for _, building in buildings.iterrows():
-            geometry = building.geometry 
+            geometry = building.geometry
             properties = building.drop('geometry').to_dict() # gets properties
 
             tooltip_content = "<br>".join([f"{key}: {value}" for key, value in properties.items() if value is not None and value is not np.nan]) #lists all data not None in popup/tooltip
@@ -345,7 +527,7 @@ class MapCreator:
         self.nearby_buildings_water_fg = self.add_buildings_tooltips(self.nearby_buildings_water, self.nearby_buildings_water_fg, styler=self.mapStyler.style_nearby_buildings_water, extra_data = {'Digital Twin Name': self.name, 'Building Type': 'within water buffer'})
         self.nearby_buildings_water_fg.add_to(self.m)
 
-    def render_buffer_areas(self, water_buffer=True, road_buffer=True):
+    def render_buffer_areas(self, water_buffer=True, road_buffer=True, weather_report=True):
         self.utm_zone = int((self.longitude + 180) // 6) + 1
         self.is_northern_hemisphere = self.latitude >= 0
         self.epsg_code = 32600 + self.utm_zone if self.is_northern_hemisphere else 32700 + self.utm_zone
@@ -355,7 +537,7 @@ class MapCreator:
         if road_buffer==True:
             self.render_buffer_area_road()
 
-    def save_map(self, interactive_marker=True, camera_simulation=True):
+    def save_map(self, interactive_marker=True, camera_simulation=True, weather_report=True):
         """Saves barebones map with static info"""
         self.m.save(self.map_name)
 
@@ -367,6 +549,11 @@ class MapCreator:
             self.javaScriptInjector.inject_camera_simulation_script(self.map_name, camera_latitude=51.176858, camera_longitude=5.882079, direction=-60, width=94, reach=200, camera_name='camera 1 red', video_source='https://www.youtube.com/embed/4qOxFyZLcl0?si=VO9YbHXW7mDSENHO', cone_outline_color='red', cone_fill_color='lightred', camera_outline_color='blue', camera_fill_color='lightblue')
             self.javaScriptInjector.inject_camera_simulation_script(self.map_name, camera_latitude=51.179512, camera_longitude=5.878201, direction=180, width=94, reach=200, camera_name='camera 2 blue', video_source='https://www.youtube.com/embed/4qOxFyZLcl0?si=VO9YbHXW7mDSENHO', cone_outline_color='blue', cone_fill_color='lightblue', camera_outline_color='red', camera_fill_color='lightred')
             self.javaScriptInjector.inject_camera_simulation_script(self.map_name, camera_latitude=51.184346, camera_longitude=5.876827, direction=278, width=137, reach=800, camera_name='camera 3 green', video_source='https://www.youtube.com/embed/lffpBLDQqqc?si=H6um6OemAf8UQc56', cone_outline_color='green', cone_fill_color='lightgreen', camera_outline_color='purple', camera_fill_color='lightpurple')
+        if weather_report == True:
+            # Should be changed to a more secure way of storing the API key
+            self.api_key_openweathermap = "d0160058812e1e5f9b9bdfb04c5ffca9"
+            self.javaScriptInjector.inject_weather_report_script(self.map_name, self.latitude, self.longitude, self.api_key_openweathermap)
+    
         return self.map_name
 
     def create_detailed_map(self):
@@ -414,5 +601,5 @@ class MapCreator:
         return saved_map_name
     
 # callng the stuff
-map_creator_1 = MapCreator(51.1797305,5.8812762,"Boschmolenplas", 2000, 150, 20)
+map_creator_1 = MapCreator(51.1797305,5.8812762,"Boschmolenplas", 1500, 150, 20)
 map_creator_1.create_detailed_map()
